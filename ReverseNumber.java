af030d5da6c4f8db0ae80f9a9dab8b710f8fe6c8
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-02 21:36:27 +0530

    Print The Array in Reverse Order

---
+++ ReverseNumber.java
@@ -0,0 +1,10 @@
+package com.company2;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int[] array = {56,524,68251,6385536,65,6};
+        for (int i = array.length-1; i>=0;i--){
+            System.out.println(array[i]);
+        }
+    }
+}
