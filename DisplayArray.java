befe7c1833bb129fdf48949c7110b4add7b3a748
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-02 21:32:10 +0530

    Print the Elements of Array

---
+++ DisplayArray.java
@@ -0,0 +1,10 @@
+package com.company2;
+
+public class DisplayArray {
+    public static void main(String[] args) {
+        int[] array = {5, 56, 78, 85, 36, 254};
+        for (int number : array){
+            System.out.println(number);
+        }
+    }
+}
