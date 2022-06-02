2b79907768bcdf2904e2f97382aa440ecd4aa1be
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-02 21:38:12 +0530

    Print Odd Number in Array

---
+++ OddNumber.java
@@ -0,0 +1,13 @@
+package com.company2;
+
+public class OddNumber {
+    public static void main(String[] args) {
+        int[] array = {56,524,68251,6385536,65,6};
+        for (int number:array){
+            if (number%2==0) {
+            }else {
+                System.out.println(number);
+            }
+        }
+    }
+}
